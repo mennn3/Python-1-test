3e7cc6146b8a4eab6c972f3ff5b62f2548dd1da3
NaufalR <43438908+naufalrivaille@users.noreply.github.com>
2018-09-20 18:42:33 +0700

    Update datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -1,1 +1,9 @@
 # Python-1-test
+
+print ("-----------------------------------------------")
+print ("Mengetahui Tipe data saat user memasukkan kunci")
+print ("-----------------------------------------------\n")
+
+user = (input("Masukkan kata kunci : "))
+#print (("Kunci yang anda masukkan adalah : "),user, "=", (type(user)))
+print("Tipe yang di gunakan : " , user , "=" , type(user))
